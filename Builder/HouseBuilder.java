6f114e51a8592f285a1e3a59bf514fbdf292efd9
vinkims <vincekigen@gmail.com>
2023-07-14 16:23:06 +0300

    Add Builder pattern

---
+++ Builder/HouseBuilder.java
@@ -0,0 +1,12 @@
+interface HouseBuilder {
+    
+    public void buildBasement();
+
+    public void buildStructure();
+
+    public void buildRoof();
+
+    public void buildInterior();
+
+    public House getHouse();
+}
