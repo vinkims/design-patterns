575d150759402b7a64b88b3e13cd9d0de4a0764a
vinkims <vincekigen@gmail.com>
2023-07-15 10:05:45 +0300

    Add Proxy pattern

---
+++ Proxy/Client.java
@@ -0,0 +1,13 @@
+public class Client {
+    
+    public static void main(String[] args) {
+        
+        Internet internet = new ProxyInternet();
+        try {
+            internet.connectTo("geeksforgeeks.com");
+            internet.connectTo("abc.com");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ Proxy/RealInternet.java
@@ -0,0 +1,7 @@
+public class RealInternet implements Internet {
+    
+    @Override
+    public void connectTo(String serverhost) {
+        System.out.println("Connecting to " + serverhost);
+    }
+}
